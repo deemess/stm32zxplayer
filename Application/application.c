b3be7704291a61e8ad52f8d9f0b3b76a2deb7b07
deemess <anokhindm@gmail.com>
2024-08-19 20:49:12 +0300

    Bug fixes

--- Application/application.c
+++ Application/application.c
@@ -245,7 +245,7 @@ int readBlock(int blockN) { // 1 - block read, 0 - failed to read block / no blo
 
 		res = f_read(&fsrc, fileBuffer, blockSize, &br);
 		if(res != FR_OK || br == 0) {
-
+			f_close(&fsrc);
 			//unmount
 			f_mount(0, SDPath, 0);
 			return 0;
